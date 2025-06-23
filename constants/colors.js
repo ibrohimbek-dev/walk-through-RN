83811b0e05be89e7d4e1be03ed7259833d2343b0
Ibrohimbek <programming0127@gmail.com>
2025-06-24 07:17:09 +0900

    feat: 2025-06-24 - 07: 17

---
+++ constants/colors.js
@@ -0,0 +1,52 @@
+export const Colors = {
+	// Primary Purple (your brand color)
+	primary50: "#f3e8ff",
+	primary100: "#e4d9fd",
+	primary200: "#c6affc",
+	primary300: "#a281f0",
+	primary400: "#8147e0",
+	primary500: "#5721d4",
+	primary600: "#3e04c3",
+	primary700: "#2d0689",
+	primary800: "#200364",
+	primary900: "#140239",
+
+	// Accent Yellow
+	accent100: "#fff0c2",
+	accent300: "#fdd85d",
+	accent500: "#f7bc0c",
+	accent700: "#c79500",
+	accent900: "#7a5c00",
+
+	// Error Reds/Pinks
+	error50: "#fff0f3",
+	error100: "#fcc4e4",
+	error300: "#f3689d",
+	error500: "#9b095c",
+	error700: "#6a043f",
+	error900: "#3d0225",
+
+	// Neutral Grays
+	gray50: "#f9f8fa",
+	gray100: "#f0eef2",
+	gray200: "#e1dee6",
+	gray300: "#c8c4d0",
+	gray400: "#8e899a",
+	gray500: "#39324a",
+	gray600: "#2d273d",
+	gray700: "#221c30",
+	gray800: "#181225",
+	gray900: "#0e0a16",
+
+	// Success Greens
+	success50: "#e6f7ed",
+	success100: "#c8f0d9",
+	success200: "#a9e9c5",
+	success300: "#8be2b1",
+	success400: "#6ddb9d",
+	success500: "#2ecc71",
+	success600: "#27b062",
+	success700: "#1e8a4f",
+	success800: "#16643c",
+	success900: "#0e4a28",
+};
---
+++ etc/docs.js
@@ -0,0 +1,8 @@
+// 2025-06-23:
+
+// Installing React Native App:
+// npx create-expo-app --template blank react-native-project
+
+// Reset:
+// npm install ; npm start -- --reset-cache
+// npm install ; npm start
